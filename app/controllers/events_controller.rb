f252da08db47c97c6eb39579508a726e65849144
jesseborm <jesseborm@gmail.com>
2017-05-24 13:31:41 +0200

    Add Feature Specs: Events and fix events contr spec

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update]
-  # before_action :authenticate_user!, except: [:show]
+  before_action :authenticate_user!, except: [:show]
 
   def index
     @events = current_user.events
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 RSpec.describe EventsController, type: :controller do
 
   describe "GET #index" do
+
     let(:user) { create :user }
-    
+
     it "returns http success" do
+      sign_in user
       get :index
       expect(response).to have_http_status(:success)
     end
