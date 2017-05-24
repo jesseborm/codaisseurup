a4dfca6589a8f4fcee82d8c5b0026f081d9f8cae
jesseborm <jesseborm@gmail.com>
2017-05-24 11:25:27 +0200

    Add navbar specs

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe EventsController, type: :controller do
 
   describe "GET #index" do
+    let(:user) { create :user }
+    
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
